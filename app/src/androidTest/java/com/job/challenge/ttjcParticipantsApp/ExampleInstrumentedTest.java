309c19fb7671b1013399a39386bc16bf587d40e9
JowelTisso <52632590+JowelTisso@users.noreply.github.com>
2020-04-15 01:23:38 +0530

    Small modifications : 1. Package name changed in ExampleInstrumentedTest.java file

--- app/src/androidTest/java/com/job/challenge/ttjcParticipantsApp/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/job/challenge/ttjcParticipantsApp/ExampleInstrumentedTest.java
@@ -22,6 +22,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
-        assertEquals("com.example.android.coronaapitest", appContext.getPackageName());
+        assertEquals("com.job.challenge.ttjcParticipantsApp", appContext.getPackageName());
     }
 }
